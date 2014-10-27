5978fa8bcfdc557244cbcf4a48c38ee84b9b210e
bravefoot <b.v.campbell3@gmail.com>
2014-10-27 16:56:07 -0600

    Len problem

--- client/agency/ProbabilityGrid.py
+++ client/agency/ProbabilityGrid.py
@@ -22,8 +22,8 @@ class ProbabilityGrid:
 	def batchUpdate(self, x, y, filterGrid):
 		x = x + 400
 		y = 400 - y
-		width = filterGrid[0].size()
-		height = filterGrid[0][0].size()
+		width = len(filterGrid[0])
+		height = len(filterGrid[0][0])
 		for dx in range(width):
 			for dy in range(height):
 				self.update(x+dx, y+dy, filterGrid[dx][dy])
